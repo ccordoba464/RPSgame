073d5dda883c358df6c84f8bbe4b9640a5372ff2
ccordoba464 <93160733+ccordoba464@users.noreply.github.com>
2023-06-13 16:57:54 -0400

    Finish UI

--- app.js
+++ app.js
@@ -4,63 +4,78 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+  let computerSelection = getComputerChoice();
+  let computerScore = document.getElementById("cpu-score");
+  let userScore = document.getElementById("user-score");
+
+  if (computerScore.textContent === "5" || userScore.textContent === "5") {
+    computerScore.textContent = 0;
+    userScore.textContent = 0;
+  }
+
   switch (playerSelection.toLowerCase()) {
     case "rock":
       if (computerSelection.toLowerCase() === "paper") {
-        return "You Lose! Paper beats Rock";
+        updateScore("cpu-score");
+        updateResult("You Lose! Paper beats Rock");
       } else if (computerSelection.toLowerCase() === "rock") {
-        return "Try again! Same choice";
+        updateResult("Try again! Same choice");
       } else {
-        return "You Win! Rock beats Scissors";
+        updateScore("user-score");
+        updateResult("You Win! Rock beats Scissors");
       }
       break;
     case "paper":
       if (computerSelection.toLowerCase() === "paper") {
-        return "Try again! Same choice";
+        updateResult("Try again! Same choice");
       } else if (computerSelection.toLowerCase() === "rock") {
-        return "You Win! Paper beats Rock";
+        updateScore("user-score");
+        updateResult("You Win! Paper beats Rock");
       } else {
-        return "You Lose! Scissors beats Paper";
+        updateScore("cpu-score");
+        updateResult("You Lose! Scissors beats Paper");
       }
       break;
+
     case "scissors":
       if (computerSelection.toLowerCase() === "paper") {
-        return "You Win! Scissors beats Paper";
+        updateScore("user-score");
+        updateResult("You Win! Scissors beats Paper");
       } else if (computerSelection.toLowerCase() === "rock") {
-        return "You Lose! Rock beats Scissors";
+        updateScore("cpu-score");
+        updateResult("You Lose! Rock beats Scissors");
       } else {
-        return "Try again! Same choice";
+        updateResult("Try again! Same choice");
       }
       break;
   }
-}
 
-function game() {
-  let userScore = 0;
-  let computerScore = 0;
-
-  for (let i = 0; i < 5; i++) {
-    let outcome = false;
-
-    while (!outcome) {
-      let playerChoice = prompt("Rock, Paper or Scissors?");
-      let result = playRound(playerChoice, getComputerChoice());
-
-      if (result.charAt(4) === "W") {
-        userScore++;
-        outcome = true;
-      } else if (result.charAt(4) === "L") {
-        computerScore++;
-        outcome = true;
-      }
-      console.log(result);
-    }
+  let result = document.getElementById("outcome");
+  if (computerScore.textContent == "5") {
+    result.textContent = "CPU is the winner!";
+  } else if (userScore.textContent == "5") {
+    result.textContent = "You are the winner!";
   }
-
-  return userScore > computerScore
-    ? "You win the game!"
-    : "You lose the game. ";
 }
 
-//console.log(game());
+function updateScore(winner) {
+  let winnerScore = document.getElementById(winner);
+  let currentScore = +winnerScore.textContent;
+  let newScore = currentScore + 1;
+  winnerScore.textContent = newScore;
+}
+
+function updateResult(outcome) {
+  let result = document.getElementById("outcome");
+  result.textContent = outcome;
+}
+
+/* Event Listeners */
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    playRound(button.textContent);
+  });
+});
